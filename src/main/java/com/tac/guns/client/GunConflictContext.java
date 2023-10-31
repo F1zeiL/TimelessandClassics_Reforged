103bb8e3fa6603c0be22f03dc48d38123abcf42b
Fizeil <desyertro@gmail.com>
2023-10-31 17:23:52 +0800

    fix attachment dye response

--- src/main/java/com/tac/guns/client/GunConflictContext.java
+++ src/main/java/com/tac/guns/client/GunConflictContext.java
@@ -1,6 +1,9 @@
 package com.tac.guns.client;
 
 import com.tac.guns.item.GunItem;
+import com.tac.guns.item.IrDeviceItem;
+import com.tac.guns.item.ScopeItem;
+import com.tac.guns.item.SideRailItem;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.settings.IKeyConflictContext;
 import net.minecraftforge.client.settings.KeyConflictContext;
@@ -15,7 +18,11 @@ public enum GunConflictContext implements IKeyConflictContext
         @Override
         public boolean isActive()
         {
-            return !KeyConflictContext.GUI.isActive() && Minecraft.getInstance().player != null && Minecraft.getInstance().player.getHeldItemMainhand().getItem() instanceof GunItem;
+            return !KeyConflictContext.GUI.isActive() && Minecraft.getInstance().player != null &&
+                    (Minecraft.getInstance().player.getHeldItemMainhand().getItem() instanceof GunItem ||
+                            Minecraft.getInstance().player.getHeldItemMainhand().getItem() instanceof ScopeItem ||
+                            Minecraft.getInstance().player.getHeldItemMainhand().getItem() instanceof SideRailItem ||
+                            Minecraft.getInstance().player.getHeldItemMainhand().getItem() instanceof IrDeviceItem);
         }
 
         @Override
