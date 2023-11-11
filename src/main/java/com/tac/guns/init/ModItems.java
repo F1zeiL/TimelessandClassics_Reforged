bd8244e1d7f8a46bb4cab110c242a05694dae6b5
Fizeil <desyertro@gmail.com>
2023-11-12 03:09:44 +0800

    special skin set

--- src/main/java/com/tac/guns/init/ModItems.java
+++ src/main/java/com/tac/guns/init/ModItems.java
@@ -311,13 +311,13 @@ public class ModItems {
     //public static final RegistryObject<Item> ULTIMATE_CERTIFICATE = REGISTER.register("ultimate_certificate",() -> new Item(new Item.Properties().maxStackSize(16).group(GunMod.GROUP)));
 
     /* Skin */
-    public static final RegistryObject<Item> SKIN_CUSTOM = REGISTER.register("skin_custom",
-            () -> new GunSkinItem(GunSkin.create((ResourceLocation) null),new Item.Properties().maxStackSize(1).group(GunMod.SKINS)));
+//    public static final RegistryObject<Item> SKIN_CUSTOM = REGISTER.register("skin_custom",
+//            () -> new GunSkinItem(GunSkin.create((ResourceLocation) null),new Item.Properties().maxStackSize(1).group(GunMod.SKINS)));
     /*** Skin ***/
     public static final RegistryObject<Item> SKIN_AK_SPENT_BULLET = REGISTER.register("skin_ak_spent_bullet",
-            () -> new GunSkinItem(GunSkin.create("spent_bullet"), new Item.Properties().maxStackSize(1).group(GunMod.SKINS)));
+            () -> new GunSkinItem(GunSkin.create(new ResourceLocation("skin_ak_spent_bullet")), new Item.Properties().maxStackSize(1).group(GunMod.SKINS)));
     public static final RegistryObject<Item> SKIN_MP9_THUNDER = REGISTER.register("skin_mp9_thunder",
-            () -> new GunSkinItem(GunSkin.create("thunder"), new Item.Properties().maxStackSize(1).group(GunMod.SKINS)));
+            () -> new GunSkinItem(GunSkin.create(new ResourceLocation("skin_mp9_thunder")), new Item.Properties().maxStackSize(1).group(GunMod.SKINS)));
 
     /* Common Skin */
     public static final RegistryObject<Item> SKIN_BLACK = REGISTER.register("skin_black",
--- src/main/java/com/tac/guns/item/GunSkinItem.java
+++ src/main/java/com/tac/guns/item/GunSkinItem.java
@@ -33,23 +33,6 @@ public class GunSkinItem extends Item implements IgunSkin, IColored {
     }
 
     @Override
-    public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
-        if (this.isInGroup(group)) {
-            if (ModItems.SKIN_CUSTOM.get().equals(this) && NetworkModifierManager.getCustomModifiers() != null) {
-                NetworkModifierManager.getCustomModifiers().forEach((k, v)->{
-                    if(v.getSkin()!=null){
-                        ItemStack stack = new ItemStack(this);
-                        setCustomModifier(stack,v.getId());
-                        items.add(stack);
-                    }
-                });
-            }else {
-                super.fillItemGroup(group,items);
-            }
-        }
-    }
-
-    @Override
     public GunSkin getProperties() {
         return this.gunSkin;
     }
