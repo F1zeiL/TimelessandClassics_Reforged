7212241eca17938b6c85953d272a92b1e10bed8d
xjqsh <59763518+xjqsh@users.noreply.github.com>
2024-02-08 23:38:21 +0800

    init program base dye skin

---
+++ src/main/java/com/tac/guns/client/gunskin/BasicSkin.java
@@ -0,0 +1,9 @@
+package com.tac.guns.client.gunskin;
+
+import net.minecraft.util.ResourceLocation;
+
+public class BasicSkin extends GunSkin{
+    protected BasicSkin(ResourceLocation gun) {
+        super(new ResourceLocation("tac:dye_basic"), gun);
+    }
+}
